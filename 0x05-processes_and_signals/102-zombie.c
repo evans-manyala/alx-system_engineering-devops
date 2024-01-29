b27dfc40422d66087c6f8eeb6665d887f568ef03
evans-manyala <evans.manyala@gmail.com>
2024-01-29 21:07:47 +0300

    Adding syntax error fix

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,30 +4,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * infinite_while - Creates Zombie processes infinitely
+ * Return: Zero if successful
+*/
 int infinite_while(void)
 {
 	while (1)
 		sleep(1);
-        return (0);
+	return (0);
 }
-
+/**
+ * main - Program creates the main parent process.
+ * Return: Zero if successful
+*/
 int main(void)
 {
 	pid_t pid;
-   int i;
+	int x;
 
-   for (i = 0; i < 5; i++) {
-       pid = fork();
+	for (x = 0; x < 5; x++)
+	{
+		pid = fork();
 
-       if (pid < 0) {
-           perror("fork");
-           exit(1);
-       } else if (pid == 0) {
-           /* Child process */
-           exit(0); /* Child exits immediately, creating a zombie */
-       } else {
-           /* Parent process */
-           printf("Zombie process created, PID: %d\n", pid);
-       }
-   }
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(1);
+		}
+		else if (pid == 0)
+		{
+			/* Child process */
+			exit(0); /* Child exits immediately, creating a zombie */
+		}
+		else
+		{
+			/* Parent process */
+			printf("Zombie process created, PID: %d\n", pid);
+		}
+	}
 }
