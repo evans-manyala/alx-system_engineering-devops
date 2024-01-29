3e5cc7b8f8841f72cfe95e4a7726480f54d82959
evans-manyala <evans.manyala@gmail.com>
2024-01-29 20:57:18 +0300

    Adding script code

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -6,7 +6,28 @@
 
 int infinite_while(void)
 {
-        while (1)
-            sleep(1);
+	while (1)
+		sleep(1);
         return (0);
+}
+
+int main(void)
+{
+	pid_t pid;
+   int i;
+
+   for (i = 0; i < 5; i++) {
+       pid = fork();
+
+       if (pid < 0) {
+           perror("fork");
+           exit(1);
+       } else if (pid == 0) {
+           /* Child process */
+           exit(0); /* Child exits immediately, creating a zombie */
+       } else {
+           /* Parent process */
+           printf("Zombie process created, PID: %d\n", pid);
+       }
+   }
 }
