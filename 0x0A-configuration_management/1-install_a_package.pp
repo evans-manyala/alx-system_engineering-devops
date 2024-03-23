fd6a1591cbf8989c05d9771685087e366a0ebbda
evans-manyala <evans.manyala@gmail.com>
2024-03-23 10:40:48 +0300

    Adding flask version script

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+#Puppet install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+#Puppet install Werkzeug
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
