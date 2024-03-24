227f4991cd36a6388ecdd7b6ad9f6d9b4d02e2ca
evans-manyala <evans.manyala@gmail.com>
2024-03-24 20:03:48 +0300

    Adding erorr fix

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Exec resource to kill the process
 exec { 'kill_killmenow_process':
   command => '/usr/bin/pkill -f killmenow',
-  onlyif => '/usr/bin/pgrep killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow',
 }
