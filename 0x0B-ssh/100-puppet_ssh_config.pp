7ae5437f6ad8e6d05385e0de16f4f2f24b70ad01
evans-manyala <evans.manyala@gmail.com>
2024-03-25 09:43:40 +0300

    Adding error fixes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,7 @@
 # This puppet config script sets up your client SSH configuration
 # file so that you can connect to a server without typing a password
 
-$conf = "IdentityFile ~/.ssh/school
-PubKeyAuthentication yes
-PasswordAuthentication no
-"
-
-file {'/etc/ssh/ssh_config':
-  ensure  => present,
-  content => $conf,
+exec {'Puppet Configuration':
+  path    => '/usr/bin:/bin',
+  command => 'echo -e "Host 52.207.70.238\n	IdentityFile ~/.ssh/school\n	PasswordAuthentication no" >> /etc/ssh/ssh_config',
 }
