88f915d0d97f6ef39f8c08984860478bc76597cc
evans-manyala <evans.manyala@gmail.com>
2024-04-22 22:04:32 +0300

    Fixing errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,28 +5,40 @@ for a given employee ID, returns information about
 his/her TODO list progress.
 """
 
-import re
 import requests
-import sys
+from sys import argv
 
-URL = "https://jsonplaceholder.typicode.com"
+
+def fetch_todo_progress(employee_id):
+    """Fetch the employee id"""
+    URL = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    response = requests.get(URL)
+    return response.json()
+
+
+def fetch_user(employee_id):
+    """Fetch  the user associated with the resource"""
+    URL = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(URL)
+    user_data = response.json()
+    name = user_data.get('name')
+    return name
+
+
+def display_tasks(employee_id):
+    """Show the completed tasks for a user"""
+    tasks_data = fetch_todo_progress(employee_id)
+    num_of_tasks = len(tasks_data)
+    completed_tasks = sum(task['completed'] for task in tasks_data)
+    return completed_tasks, num_of_tasks
+
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(URL, id)).json()
-            task_req = requests.get('{}/todos'.format(URL)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+    employee_id = argv[1]
+    complete_task, sum_tasks = display_tasks(employee_id)
+    user = fetch_user(employee_id)
+
+    print(f"Employee {user} is done with tasks({complete_task}/{sum_tasks}):")
+    for task in fetch_todo_progress(employee_id):
+        if task['completed']:
+            print(f"\t {task['title']}")
