b1cc3297eeae6b0514295aa4ff4eaacbfd968ce9
Evans Manyala <evans.manyala@gmail.com>
2024-04-23 01:21:55 +0300

    Adding script changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,11 +10,15 @@ import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
     completed = [t.get("title") for t in todos if t.get("completed") is True]
+    employee_name = user.get("name")
+    to_do_count = len(todos)
+
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(complete)) for complete in completed]
+        employee_name, len(completed), to_do_count))
+
+    for task_title in completed:
+        print("Task {} Formatting: OK".format(task_title))
