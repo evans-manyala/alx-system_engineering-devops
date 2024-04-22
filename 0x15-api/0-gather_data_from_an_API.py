e94502be652dd416eafc8e74e5b97389f4a09947
Evans Manyala <evans.manyala@gmail.com>
2024-04-23 00:15:45 +0300

    Adding script error fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,59 +8,12 @@ his/her TODO list progress.
 import requests
 import sys
 
-URL = "https://jsonplaceholder.typicode.com/users/"
-
-
-def fetch_todo_progress(employee_id):
-    """Get the employee id"""
-    return requests.get(f"{URL}/{user_id}/todos").json()
-
-
-def get_completed_tasks(tasks: "list[dict]") -> "list[dict]":
-    """Returns all the TODO tasks that the user has completed.
-    """
-    return [task for task in tasks if task.get("completed") is True]
-
-
-def print_completed_tasks(
-        user: str, num_of_tasks: int, completed_tasks: "list[dict]"
-) -> None:
-    """
-    Shows the completed tasks for a given user.
-    """
-    print(
-        f"Employee {user} is done with "
-        f"tasks({len(completed_tasks)}/{num_of_tasks}):"
-    )
-
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
-
-
-def get_name(user_id: int) -> "str | None":
-    """
-     Fetches name associated with the user ID from API.
-    """
-    return requests.get(f"{URL}/{user_id}").json().get("name", None)
-
-
-def get_username(user_id):
-    """
-    Retrieves the name associated with the given user ID from an API.
-    """
-    return requests.get(f"{URL}/{user_id}").json().get("username", None)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.stderr.write(f"Usage: {sys.argv[0]} <user_id>\n")
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    user = get_name(sys.argv[1])
-    if user is None:
-        sys.stderr.write("Invalid user id.\n")
-        sys.exit(1)
-
-    todos = get_todos(sys.argv[1])
-    completed_tasks = get_completed_tasks(todos)
-    print_completed_tasks(user, len(todos), completed_tasks)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
