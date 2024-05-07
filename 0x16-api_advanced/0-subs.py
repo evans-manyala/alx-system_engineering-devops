8747a7d482504f3512cf294d5f6cd89ef694a12f
Evans Manyala <evans.manyala@gmail.com>
2024-05-07 21:35:28 +0300

    Affing error fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,14 +7,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    response = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        allow_redirects=False,
-        headers={"user_agent": "Chrome/13.0"},
-    )
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Chrome/13.0"}
 
-    return (
-        response.json()["data"]["subscribers"]
-        if response.status_code == 200
-        else 0
-    )
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json().get("data", {})
+        subscribers = data.get("subscribers", 0)
+        return subscribers
+    except requests.RequestException as e:
+        print(f"Error occurred: {e}")
+        return 0
