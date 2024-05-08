21ae6be1f20d24ed7a3630c97b48dbebe7e8ee77
Evans Manyala <evans.manyala@gmail.com>
2024-05-08 08:19:06 +0300

    Fixing errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Chrome/13.0"}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json().get("data", {})
-        subscribers = data.get("subscribers", 0)
-        return subscribers
-    except requests.RequestException as e:
-        print(f"Error occurred: {e}")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    result = response.json().get("data")
+    return result.get("Subscribers")
